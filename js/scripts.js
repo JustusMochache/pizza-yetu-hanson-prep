7fb72ffede0ca6b757e274059e2ce4c1accd83af
moringaschool <justusm10@gmail.com>
2021-05-02 02:12:13 +0300

    modify javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // business logic
 
-function Order(type, size, crust, topping){
+function Order(type, size, crust, topping) {
     this.type = type;
     this.size = size;
     this.crust = crust;
@@ -8,54 +8,72 @@ function Order(type, size, crust, topping){
 }
 //prototype is like a photocopy of origin document
 
-Order.prototype.fullOrder = function(){
-     return this.type + " With a crust of " + this.crust + " and with " + this.topping + "as topping";
-    };
+Order.prototype.fullOrder = function () {
+    return this.type + " with the crust of " + this.crust + " and " + this.topping + " as topping.";
+};
+//  margharita with a crust of crispy and with paperoni as topping
 
-    //  margharita with a crust of crispy and with paperoni as topping
-
-function Total(price, quantity, delivery){
+function Total(price, quantity, delivery) {
     this.price = price;
     this.quantity = quantity;
     this.delivery = delivery;
 }
 
-Total.prototype.finalTotal = function(){
+
+Total.prototype.finalTotal = function () {
     return this.price * this.quantity + this.delivery;
 };
 
-
+//variables
 var sizePrice = [1200, 900, 600];
-var deliveryPrice = [ 0, 150];
-var crustPrice = [300, 400, 500,600]
+var deliverPrices = [0, 150];
+
 
 // user interface logic
 
-// use Jquery in user logic
-$(document).ready(function(event){
-    // event.preventDefault();
+//we will use jquery for our logic
+$(document).ready(function () {
+    $('form#myForm').submit(function (event) {
 
-    var pizzaType = $('#type').val();
-    var pizzaSize = parseInt('#size').val();
-    var pizzaCrust = $('#crust').val();
-    var pizzaTop = $('#top').val();
-    var pizzaPick = parseInt('#pick').val();
-    var price = sizePrice[pizzaSize-1];
-    var deliveryCost = deliveryPrice[pizzaPick-1];
+        var pizzaType = $('#type').val();
 
+        var pizzaSize = parseInt($('#size').val());
 
-    newOrder = new Order(p)
+        var pizzaCrust = $('#crust').val();
 
+        var pizzaTop = $('#top').val();
 
+        var pizzaQty = parseInt($('#qty').val());
 
+        var pizzaPick = parseInt($('#pick').val());
 
 
+        var price = sizePrice[pizzaSize - 1];
+
+
+        var DeliveryCost = deliverPrices[pizzaPick - 1];
 
 
 
+        newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
 
+        newTotal = new Total(price, pizzaQty, DeliveryCost);
 
+        if (pizzaPick === 1) {
+            alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+            alert("your bill is: " + newTotal.finalTotal());
+        } else {
+                //prompt is to insert a string
+                prompt("Enter where you want your pizza to be delivered");
 
-})
+                alert("Your order has been received and it will be delivered. Continue to see your order details");
 
+                alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+                alert("your bill is: " + newTotal.finalTotal());
+           
+            
+        }
 
+    });
+
+});
